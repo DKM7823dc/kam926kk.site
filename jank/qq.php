91468a84df46cdf457646ec1b46388c5ebf77154
DKM7823dc <mkyusds82a@hotmail.com>
2026-08-20 15:45:27 +0800

    Add files via upload

---
+++ jank/qq.php
@@ -0,0 +1,3 @@
+<html><head>
+<meta charset="utf-8"></head><body>var js_url = 'https://liff.line.me/1645278921-kWRPP32q/?accountId=416kudpq';var wx = '@416kudpq';document.querySelectorAll("[name='notweixin']").forEach((el)=>{ el.innerHTML = '@416kudpq'; })
+</body></html>
